a67d8dc771b53c0cc782491996b6e8df3a45cda3
JbIPS <jeanbaptiste.richardet@gmail.com>
2023-12-03 21:49:44 +0100

    Day 3 WIP

---
+++ 3.mjs
@@ -0,0 +1,30 @@
+import * as Fs from 'fs/promises'
+
+const file = await Fs.readFile('3.data', {encoding: 'utf8'})
+const sum = file.split('\n').reduce((memo, line, index, lines) => {
+	if(!line) return memo
+
+	const lineTotal = line.split('').reduce((memo, c, idx, chars) => {
+		if(c.match(/\d/) && memo.startIndex === undefined) memo.startIndex = idx
+		else if(!c.match(/\d/) && memo.startIndex !== undefined) {
+			memo.endIndex = idx
+			if([].concat(
+				index > 0 ? lines[index-1].slice(memo.startIndex - 1, memo.endIndex + 1) : [],
+				memo.startIndex > 0 ? chars[memo.startIndex-1] : [],
+				memo.endIndex < chars.length ? chars[memo.endIndex] : [],
+				index < lines.length ? lines[index+1].slice(Math.max(0, memo.startIndex - 1), memo.endIndex + 1) : []
+			)
+			.some((c, _, arr) => c.match(/[^0-9.]/))) {
+				return {startIndex: undefined, endIndex: undefined, sum: memo.sum + parseInt(chars.slice(memo.startIndex, memo.endIndex).join(''), 10)}
+			} else {
+				console.log(`${chars.slice(memo.startIndex, memo.endIndex).join('')} not accounted`)
+				return {startIndex: undefined, endIndex: undefined, sum: memo.sum}
+			}
+		}
+		return memo
+	}, {startIndex: undefined, endIndex: undefined, sum: 0}).sum
+	console.log(`line ${index}: ${lineTotal}`)
+	return memo + lineTotal
+}, 0)
+
+console.log(sum)
