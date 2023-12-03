914671e38abac49fb3c3d8d97e2d88260b9ea565
JbIPS <jeanbaptiste.richardet@gmail.com>
2023-12-03 23:00:10 +0100

    Day 3 - part 2

--- 3.mjs
+++ 3.mjs
@@ -1,30 +1,45 @@
 import * as Fs from 'fs/promises'
 
 const file = await Fs.readFile('3.data', {encoding: 'utf8'})
-const sum = file.split('\n').reduce((memo, line, index, lines) => {
+function findAsterisk(asterisks, x, y, number) {
+	const ast = asterisks.find(a => a.y === y && a.x === x)
+	if(ast == null) asterisks.push({x, y, numbers:[number]})
+	else ast.numbers.push(number)
+}
+const asterisks = file.split('\n').reduce((memo, line, index, lines) => {
 	if(!line) return memo
 
-	const lineTotal = line.split('').concat(['.']).reduce((memo, c, idx, chars) => {
+	const {asterisks: lineAsterisks} = line.split('').concat(['.']).reduce((memo, c, idx, chars) => {
 		if(c.match(/\d/) && memo.startIndex === undefined) memo.startIndex = idx
 		else if(!c.match(/\d/) && memo.startIndex !== undefined) {
 			memo.endIndex = idx
-			if([].concat(
-				index > 0 ? lines[index-1].slice(Math.max(0, memo.startIndex - 1), Math.min(chars.length, memo.endIndex + 1)) : [],
-				memo.startIndex > 0 ? chars[memo.startIndex-1] : [],
-				memo.endIndex < chars.length ? chars[memo.endIndex] : [],
-				index < lines.length ? lines[index+1].slice(Math.max(0, memo.startIndex - 1), Math.min(chars.length, memo.endIndex + 1)) : []
-			)
-			.some(c => c.match(/[^0-9.]/))) {
-				return {startIndex: undefined, endIndex: undefined, sum: memo.sum + parseInt(chars.slice(memo.startIndex, memo.endIndex).join(''), 10)}
-			} else {
-				// console.log(`${chars.slice(memo.startIndex, memo.endIndex).join('')} not accounted`)
-				return {startIndex: undefined, endIndex: undefined, sum: memo.sum}
+			const number = parseInt(chars.slice(memo.startIndex, memo.endIndex).join(''), 10)
+			if(index > 0) {
+				const y = index-1
+				const offset = memo.startIndex === 0 ? 0 : memo.startIndex-1
+				const up = lines[y].slice(offset, Math.min(chars.length, memo.endIndex + 1))
+				for(let m of up.matchAll(/\*/g)) {
+					findAsterisk(memo.asterisks, offset + m.index, y, number)
+				}
 			}
+			if(memo.startIndex > 0 && chars[memo.startIndex-1] === '*') {
+				findAsterisk(memo.asterisks, memo.startIndex-1, index, number)
+			}
+			if(memo.endIndex < chars.length && chars[memo.endIndex] === '*') {
+				findAsterisk(memo.asterisks, memo.endIndex, index, number)
+			}
+			if(index < lines.length) {
+				const y = index+1
+				const offset = memo.startIndex === 0 ? 0 : memo.startIndex-1
+				const down = lines[y].slice(offset, Math.min(chars.length, memo.endIndex + 1))
+				for(let m of down.matchAll(/\*/g)) {
+					findAsterisk(memo.asterisks, offset + m.index, y, number)
+				}
+			}
+			return {startIndex: undefined, endIndex: undefined, asterisks: memo.asterisks}
 		}
 		return memo
-	}, {startIndex: undefined, endIndex: undefined, sum: 0}).sum
-	// console.log(`line ${index}: ${lineTotal}`)
-	return memo + lineTotal
-}, 0)
-
-console.log(sum)
+	}, {startIndex: undefined, endIndex: undefined, asterisks: memo})
+	return lineAsterisks
+}, [])
+console.log(asterisks.reduce((memo, a) => memo + (a.numbers.length === 2 ? a.numbers[0]*a.numbers[1] : 0), 0))
