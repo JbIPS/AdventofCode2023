cfc4e9796034a8dad3d8de3968b1362b4f520e36
JbIPS <jeanbaptiste.richardet@gmail.com>
2023-12-03 21:57:24 +0100

    Fix part 1

--- 3.mjs
+++ 3.mjs
@@ -4,26 +4,26 @@ const file = await Fs.readFile('3.data', {encoding: 'utf8'})
 const sum = file.split('\n').reduce((memo, line, index, lines) => {
 	if(!line) return memo
 
-	const lineTotal = line.split('').reduce((memo, c, idx, chars) => {
+	const lineTotal = line.split('').concat(['.']).reduce((memo, c, idx, chars) => {
 		if(c.match(/\d/) && memo.startIndex === undefined) memo.startIndex = idx
 		else if(!c.match(/\d/) && memo.startIndex !== undefined) {
 			memo.endIndex = idx
 			if([].concat(
-				index > 0 ? lines[index-1].slice(memo.startIndex - 1, memo.endIndex + 1) : [],
+				index > 0 ? lines[index-1].slice(Math.max(0, memo.startIndex - 1), Math.min(chars.length, memo.endIndex + 1)) : [],
 				memo.startIndex > 0 ? chars[memo.startIndex-1] : [],
 				memo.endIndex < chars.length ? chars[memo.endIndex] : [],
-				index < lines.length ? lines[index+1].slice(Math.max(0, memo.startIndex - 1), memo.endIndex + 1) : []
+				index < lines.length ? lines[index+1].slice(Math.max(0, memo.startIndex - 1), Math.min(chars.length, memo.endIndex + 1)) : []
 			)
-			.some((c, _, arr) => c.match(/[^0-9.]/))) {
+			.some(c => c.match(/[^0-9.]/))) {
 				return {startIndex: undefined, endIndex: undefined, sum: memo.sum + parseInt(chars.slice(memo.startIndex, memo.endIndex).join(''), 10)}
 			} else {
-				console.log(`${chars.slice(memo.startIndex, memo.endIndex).join('')} not accounted`)
+				// console.log(`${chars.slice(memo.startIndex, memo.endIndex).join('')} not accounted`)
 				return {startIndex: undefined, endIndex: undefined, sum: memo.sum}
 			}
 		}
 		return memo
 	}, {startIndex: undefined, endIndex: undefined, sum: 0}).sum
-	console.log(`line ${index}: ${lineTotal}`)
+	// console.log(`line ${index}: ${lineTotal}`)
 	return memo + lineTotal
 }, 0)
 
